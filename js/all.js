57a7f84c77c6aa6298c5b637eed1089ebe2365ba
ngoctram108 <154338898+ngoctram108@users.noreply.github.com>
2023-12-30 22:19:20 +0700

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -146,3 +146,11 @@ function changeQuantity($idProduct, $type){
     // reload html view cart
     addCartToHTML();
 }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
